62dff9be69a6bdf325251b71450b8148c6ec62e0
megumimyo <yasouo@proton.me>
2024-05-02 21:33:17 +0530

    undo

--- home/models.py
+++ home/models.py
@@ -33,10 +33,11 @@ class Notes(models.Model):
         return f"{self.title} {self.content} {self.shareid}"
 
 
-class Permission(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    note = models.ForeignKey(Notes, on_delete=models.CASCADE)
-    can_write = models.BooleanField(default=False)
+# TODO:
+# class Permission(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     note = models.ForeignKey(Notes, on_delete=models.CASCADE)
+#     can_write = models.BooleanField(default=False)
 
-    def __str__(self):
-        return f"{self.user.username} - {self.note.title} - Can Write: {self.can_write}"
+#     def __str__(self):
+#         return f"{self.user.username} - {self.note.title} - Can Write: {self.can_write}"
